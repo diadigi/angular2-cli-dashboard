c3daf827dff8df7ecc30e6be8b24c3b1bb2a6236
diadigi <diana.r.nicula@gmail.com>
2016-10-18 20:46:13 +0100

    refactor: accordion data via service

--- src/app/navs/navs.component.ts
+++ src/app/navs/navs.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
 
+import {DataListService} from '../shared/data-list/data-list.service';
+
 @Component({
   selector: 'app-navs',
   templateUrl: './navs.component.html',
@@ -9,12 +11,19 @@ import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbAccordionConfig]
 })
 export class NavsComponent implements OnInit {
+  accordionPanels: any[] = [];
+  errorMessage: string;
 
-  constructor(config: NgbAccordionConfig) {
+  constructor(config: NgbAccordionConfig, public dataListService: DataListService) {
     config.closeOthers = false;
   }
 
   ngOnInit() {
+    this.dataListService.get('accordion')
+      .subscribe(
+        accordionPanels => this.accordionPanels = accordionPanels,
+        error =>  this.errorMessage = <any>error
+      );
   }
 
 }
--- src/app/navs/navs.module.ts
+++ src/app/navs/navs.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NavsComponent } from './navs.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import {DataListService} from '../shared/data-list/data-list.service';
+
 @NgModule({
   imports: [
     CommonModule,
     NgbModule.forRoot()
   ],
   declarations: [NavsComponent],
-  exports: [NavsComponent]
+  exports: [NavsComponent],
+  providers: [DataListService]
 })
 export class NavsModule { }
