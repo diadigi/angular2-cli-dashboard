0c82809f0b6179cc226ef2be3ea3cd1045134bbb
diadigi <diana.r.nicula@gmail.com>
2016-10-18 14:31:08 +0100

    feat: incipient navs route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavsComponent } from './navs/navs.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'navs',
+    component: NavsComponent
   }
 ];
