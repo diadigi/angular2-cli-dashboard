0c84589199b9cebc3e742466232e420bf5a371e0
diadigi <diana.r.nicula@gmail.com>
2016-10-18 22:18:54 +0100

    feat: tab and pills navigation

--- src/app/navs/navs.component.spec.ts
+++ src/app/navs/navs.component.spec.ts
@@ -3,13 +3,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { NavsComponent } from './navs.component';
 import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
+import {NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
 
 
 describe('Component: Navs', () => {
-  let config = new NgbAccordionConfig;
+  let accConfig = new NgbAccordionConfig;
+  let tsConfig = new NgbTabsetConfig;
 
   it('should create an instance', () => {
-    let component = new NavsComponent(config);
+    let component = new NavsComponent(accConfig, tsConfig);
     expect(component).toBeTruthy();
   });
 });
--- src/app/navs/navs.component.ts
+++ src/app/navs/navs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
+import {NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
 
 import {DataListService} from '../shared/data-list/data-list.service';
 
@@ -8,20 +9,35 @@ import {DataListService} from '../shared/data-list/data-list.service';
   selector: 'app-navs',
   templateUrl: './navs.component.html',
   styleUrls: ['./navs.component.css'],
-  providers: [NgbAccordionConfig]
+  providers: [NgbAccordionConfig, NgbTabsetConfig]
 })
 export class NavsComponent implements OnInit {
   accordionPanels: any[] = [];
+  tabset: any[] = [];
   errorMessage: string;
 
-  constructor(config: NgbAccordionConfig, public dataListService: DataListService) {
-    config.closeOthers = false;
+  constructor(accConfig: NgbAccordionConfig, tsConfig: NgbTabsetConfig, public dataListService: DataListService) {
+    accConfig.closeOthers = false;
+    tsConfig.type = "tabs";
   }
 
   ngOnInit() {
+    this.getAccordionData();
+    this.getTabsetData();
+  }
+
+  getAccordionData() {
     this.dataListService.get('accordion')
       .subscribe(
         accordionPanels => this.accordionPanels = accordionPanels,
+        error =>  this.errorMessage = <any>error
+      );
+  }
+
+  getTabsetData() {
+    this.dataListService.get('tabset')
+      .subscribe(
+        tabset => this.tabset = tabset,
         error =>  this.errorMessage = <any>error
       );
   }
