b4500211d26d2bcbacebf9ba8ceb3a429d27cdd6
diadigi <diana.r.nicula@gmail.com>
2016-10-18 18:30:32 +0100

    test: updates navs component test suite

--- src/app/navs/navs.component.spec.ts
+++ src/app/navs/navs.component.spec.ts
@@ -2,10 +2,14 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { NavsComponent } from './navs.component';
+import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 describe('Component: Navs', () => {
+  let config = new NgbAccordionConfig;
+
   it('should create an instance', () => {
-    let component = new NavsComponent();
+    let component = new NavsComponent(config);
     expect(component).toBeTruthy();
   });
 });
