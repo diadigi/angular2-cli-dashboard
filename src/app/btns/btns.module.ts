5fc28c65a5357257dee261a4946e22a07f34d664
diadigi <diana.r.nicula@gmail.com>
2016-10-19 14:25:08 +0100

    feat: added font awesome icons

--- src/app/btns/btns.module.ts
+++ src/app/btns/btns.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BtnsComponent } from './btns.component';
 
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    Angular2FontawesomeModule
   ],
   declarations: [BtnsComponent],
   exports: [BtnsComponent]
