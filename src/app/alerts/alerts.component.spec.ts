dada8d96fb73f0061fb319d5b21c2cc374504292
diadigi <diana.r.nicula@gmail.com>
2016-10-20 14:29:22 +0100

    feat: finished implementation

--- src/app/alerts/alerts.component.spec.ts
+++ src/app/alerts/alerts.component.spec.ts
@@ -3,16 +3,43 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AlertsComponent } from './alerts.component';
 
+import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
+
 describe('Component: Alerts', () => {
+  let aConfig = new NgbAlertConfig;
+  let component = new AlertsComponent(aConfig);
+
   describe('Instatiation:', () => {
-    let component = new AlertsComponent();
-    
     it('should create an instance', () => {
       expect(component).toBeTruthy();
     });
 
     it('should set cardCollapsed', () => {
       expect(component.cardCollapsed).toBeTruthy();
+    });
+
+    it('should set selfClosingVisible', () => {
+      expect(component.selfClosingVisible).toBeFalsy();
+    });
+  });
+
+  describe('Method: showSelfClosing', () => {
+    beforeAll(() => {
+      component.showSelfClosing();
+    });
+
+    it('should update selfClosingVisible', () => {
+        expect(component.selfClosingVisible).toBeTruthy();
+    });
+  });
+
+  describe('Method: closeSelfClosing', () => {
+    beforeAll(() => {
+      component.closeSelfClosing();
+    });
+
+    it('should update selfClosingVisible', () => {
+        expect(component.selfClosingVisible).toBeFalsy();
     });
   });
 });
--- src/app/alerts/alerts.component.ts
+++ src/app/alerts/alerts.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
-  styleUrls: ['./alerts.component.css']
+  styleUrls: ['./alerts.component.css'],
+  providers: [ NgbAlertConfig ]
 })
 export class AlertsComponent implements OnInit {
+  private selfClosingTimeout: any;
+
   public cardCollapsed: any[] = [];
+  public selfClosingVisible: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(alertConfig: NgbAlertConfig) {
+    alertConfig.type = 'info';
+    alertConfig.dismissible = true;
   }
 
+  ngOnInit() { }
+
+  public showSelfClosing(): void {
+    this.selfClosingVisible = true;
+    this.selfClosingTimeout = setTimeout(() => this.selfClosingVisible = false, 5000);
+  }
+
+  public closeSelfClosing(): void {
+    this.selfClosingVisible = false;
+    clearTimeout(this.selfClosingTimeout);
+  }
 }
